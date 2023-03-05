d1d656afd61c66fe6b3b6b50e15fc10d3f9e546a
Uzmanist <69589887+Uzmanist@users.noreply.github.com>
2023-03-05 22:15:42 +0300

    Bot Ayarları
    v1.0

---
+++ config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  Prefix: "", //Prefix 
+  Owners: [""], //Sahip ID
+  Token: "", //Bot token
+  mongoPass: "", //MongoUri (MongoDB URL)
+  Error_log_channel: "", //Hata log kanalý ID
+  Auth_log_channel: "", //Yetkilendirme log kanalý ID
+  oauth_link: "", //Yetkilendirme URL (identify ve guilds.join)
+  client_id: "", //Client ID
+  logchannel: "", //Ana log kanal Webhook URL
+  autoroleserver: "", //Sunucuda otorol
+  autoroleid: "", //otorolün ID
+  client_secret: "", //Client secret
+  redirect_uri: "", //Web URL (Yönlendirme)
+}
